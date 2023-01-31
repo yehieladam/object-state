d8b287c40b7a665865b517e9342cb5c5ae2e4962
Yehiel_Adam <yehieladam@gmail.com>
2023-01-31 13:38:01 +0200

    31.1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import './App.css';
 import { useState } from 'react';
 function App() {
   const[toDo, setToDo]= useState('');
-  const[toDoList,setToDoList] = useState(['Buy milk','Go to a Movie','Buy Coffee']);
+  const[toDoList,setToDoList] = useState(['חלב','קפה','עוגיות']);
+
+
+  function DeleteItem(index) {
+    setToDoList(toDoList.filter((_, i) => i !== index))
+  }
+
 
 function HendeleTodoChange(e){
   setToDo(e.target.value)
@@ -10,21 +16,23 @@ function HendeleTodoChange(e){
 
 function AddItem(){
   setToDoList([...toDoList,toDo])
+  setToDo('')
 }
 
   return (
     <div className="App">
+
       <input value = {toDo} onChange={HendeleTodoChange} />
-      <button onClick={AddItem}> Add to List </button>
-      <h3>List of To Do List</h3>
+      <button onClick={AddItem} > Add to List </button>
+      <h3>רשימת קניות </h3>
       <ul>
-        { 
-        toDoList.map(
-          toDo => (<li>{toDo}</li>)
-        )
-
+        {
+          toDoList.map((toDo, index) => (
+            <li key={index}>{toDo}
+              <button onClick={() => DeleteItem(index)}>Delete</button>
+            </li>
+          ))
         }
-
       </ul>
     </div>
   );
